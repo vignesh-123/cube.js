72894fe1166b88de586e39cd7f70365cba94d3ff
Igor Lukanin <me@igorlukanin.io>
2020-09-09 23:06:41 +0300

    Update frontend

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dd3fc3b4f54d29be6bff855ec675b08.js"
+  "/precache-manifest.1c69f8c17c9cea07f3d86a2d8285d947.js"
 );
 
 self.addEventListener('message', (event) => {
