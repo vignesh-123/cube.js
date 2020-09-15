d69168793e6f4a91fb5b0b24efc7df08ef76edd0
Dmitry Patsura <talk@dmtry.me>
2020-09-15 11:26:56 +0300

    Feature(cubejs-cli): Ask question about database, if user forget to specify it with -d flag

--- packages/cubejs-cli/cubejsCli.js
+++ packages/cubejs-cli/cubejsCli.js
@@ -11,6 +11,8 @@ const os = require('os');
 const chalk = require('chalk');
 const spawn = require('cross-spawn');
 const crypto = require('crypto');
+const inquirer = require('inquirer');
+const Drivers = require('@cubejs-backend/server-core/core/DriverDependencies');
 
 const Config = require('./Config');
 const templates = require('./templates');
@@ -52,15 +54,20 @@ const logStage = (stage) => {
 const createApp = async (projectName, options) => {
   const template = options.template || 'express';
   const createAppOptions = { projectName, dbType: options.dbType, template };
+
   event('Create App', createAppOptions);
+
   if (!options.dbType) {
-    await displayError([
-      'You must pass an application name and a database type (-d).',
-      '',
-      'Example: ',
-      ' $ cubejs create hello-world -d postgres'
-    ], createAppOptions);
+    const prompt = await inquirer.prompt([{
+      type: 'list',
+      name: 'dbType',
+      message: 'Select database',
+      choices: Object.keys(Drivers)
+    }]);
+
+    options.dbType = prompt.dbType;
   }
+
   if (await fs.pathExists(projectName)) {
     await displayError(
       `We cannot create a project called ${chalk.green(
